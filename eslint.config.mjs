c0358cc46cb0ec0bc2191573bf04ce8c28951160
Ashwanidubey15 <dubeyashwani2003@gmail.com>
2026-06-18 13:34:31 +0530

    remove type eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,13 +12,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals'),
   {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-unused-expressions': 'warn',
-      '@typescript-eslint/no-this-alias': 'warn',
-      '@typescript-eslint/no-require-imports': 'warn',
-    },
+    // rules: {
+    //   '@typescript-eslint/no-explicit-any': 'warn',
+    //   '@typescript-eslint/no-unused-vars': 'warn',
+    //   '@typescript-eslint/no-unused-expressions': 'warn',
+    //   '@typescript-eslint/no-this-alias': 'warn',
+    //   '@typescript-eslint/no-require-imports': 'warn',
+    // },
     ignores: [
       '**/node_modules/**',
       '**/.next/**',
