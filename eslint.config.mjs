b75215671ce1d0641cfe95b5aa6ed6cd2587d3c0
Ashwanidubey15 <dubeyashwani2003@gmail.com>
2026-06-18 13:43:02 +0530

    remove type eslint again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const _dirname = path.dirname(_filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
@@ -11,24 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals'),
-  {
-    // rules: {
-    //   '@typescript-eslint/no-explicit-any': 'warn',
-    //   '@typescript-eslint/no-unused-vars': 'warn',
-    //   '@typescript-eslint/no-unused-expressions': 'warn',
-    //   '@typescript-eslint/no-this-alias': 'warn',
-    //   '@typescript-eslint/no-require-imports': 'warn',
-    // },
-    ignores: [
-      '**/node_modules/**',
-      '**/.next/**',
-      '**/dist/**',
-      '**/build/**',
-      '**/generated/**',
-      '**/*.generated.*',
-      '**/prisma/generated/**',
-    ],
-  },
 ];
 
 export default eslintConfig;
